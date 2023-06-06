69827137f80c27eedd31d96073ea1309953bfc2d
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-06-06 06:15:31 +0200

    Fixed: Incorrect resource definition in output

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "id" {
   description = "The ID of the Cosmos DB Database."
-  value       = azurerm_cosmosdb_mongo_database.this.id
+  value       = azurerm_cosmosdb_sql_database.this.id
 }
 
 output "name" {
   description = "The name of the Cosmos DB Database."
-  value       = azurerm_cosmosdb_mongo_database.this.name
+  value       = azurerm_cosmosdb_sql_database.this.name
 }
