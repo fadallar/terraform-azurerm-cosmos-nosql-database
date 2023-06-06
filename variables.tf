84dae3203d14f69ccd3f62522d50586919dbe287
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-06-06 06:03:42 +0200

    Initial Commit

---
+++ variables.tf
@@ -0,0 +1,38 @@
+variable "database_name" {
+  description = "Specifies the name of the Cosmos DB Mongo Database. Changing this forces a new resource to be created."
+  type        = string
+}
+
+
+variable "resource_group_name" {
+  description = "The name of the resource group in which the Cosmos DB Account is created. Changing this forces a new resource to be created."
+  type        = string
+}
+
+variable "cosmos_db_account_name" {
+  description = "The name of the Cosmos Account to create the database within. Changing this forces a new resource to be created."
+  type        = string
+}
+
+variable "throughput" {
+  description = "The throughput of the Cosmos database (RU/s). Must be set in increments of 100. The minimum value is 400. This must be set upon database creation otherwise it cannot be updated without a manual terraform destroy-apply."
+  type        = number
+  default     = null
+  validation {
+    condition     = try(can(var.throughput / 100) && var.throughput >= 400 && var.throughput <= 1000000, true)
+    error_message = "Invalid variable: throughput"
+
+  }
+}
+
+variable "max_autoscaling_throughput" {
+  description = "The maximum autoscale throughput of the MongoDB database (RU/s). Must be between 1,000 and 1,000,000. Must be set in increments of 1,000. Conflicts with throughput."
+  type        = number
+  default     = null
+  validation {
+    condition     = try(can(var.max_autoscaling_throughput / 1000) && var.max_autoscaling_throughput >= 1000 && var.max_autoscaling_throughput <= 1000000, true)
+    error_message = "Invalid variable: max_autoscaling_throughput "
+
+  }
+
+}
